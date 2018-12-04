126913817f4a77ba31b4cd4b79c9e919743c3564
Poomdharm <poomdharmbenjasirimongkol@gmail.com>
2018-12-04 21:19:29 +0700

    get txt file with title

--- src/MainParser.java
+++ src/MainParser.java
@@ -3,16 +3,18 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.compress.compressors.FileNameUtil;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.tika.io.FilenameUtils;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.BodyContentHandler;
 
-@SuppressWarnings("deprecation")
 public class MainParser {
 	public Map<String, Object> processRecord(String url){
 		@SuppressWarnings("resource")
@@ -32,6 +34,9 @@ public class MainParser {
 					ParseContext context = new ParseContext();
 					parser.parse(input, handler, meta, context);
 					map.put("text", handler.toString().replaceAll("\n|\r|\t", " "));
+					map.put("pageCount", meta.get("xmpTPg:NPages"));
+					map.put("title", meta.get(TikaCoreProperties.TITLE));
+					map.put("status_code", res.getStatusLine().getStatusCode() + "");
 				} catch(Exception e) {
 					e.printStackTrace();
 				} finally {
--- src/Tester.java
+++ src/Tester.java
@@ -1,9 +1,19 @@
 import java.util.Map;
-
+import java.io.*;
 public class Tester {
-	public static void main(String arg[]) {
+	public static void main(String arg[]) throws FileNotFoundException, UnsupportedEncodingException {
 		MainParser webPagePdfExtractor = new MainParser();
-        Map<String, Object> extractedMap = webPagePdfExtractor.processRecord("http://www.snee.com/xml/xslt/sample.doc");
-        System.out.println(extractedMap.get("text"));
+		String url = "http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf";
+        Map<String, Object> extractedMap = webPagePdfExtractor.processRecord(url);
+        
+        
+        if(extractedMap.get("title") != null) {
+        	PrintWriter writer = new PrintWriter(extractedMap.get("title")+".txt", "UTF-8");
+        	writer.println(url);
+        	writer.println(extractedMap.get("title"));
+        	writer.println(extractedMap.get("pageCount"));
+        	writer.println(extractedMap.get("text"));
+        	writer.close();
+        }
 	}
 }
